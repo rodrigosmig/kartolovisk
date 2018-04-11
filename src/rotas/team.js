eb660d89e1481e24a5038c3af6364bb2501036fc
Rodrigo Miguel <rodrigo.santos@dce.ufpb.br>
2018-04-11 14:40:50 -0300

    informa que nenhum jogador foi adicionado ao time ao listar

--- src/rotas/team.js
+++ src/rotas/team.js
@@ -144,7 +144,7 @@ router.route('/teams/add_player')
 				team.getPlayers({newPlayer}).then(inTeam => {
 					if(inTeam.length === 0) {
 						//adiciona o jogador ao time
-						newPlayer.addTeams(team).then(teste => {
+						newPlayer.addTeams(team).then(ret => {
 							res.json({message: 'Jogador adicionado ao time'});
 						})
 					}
@@ -164,7 +164,14 @@ router.route('/teams/players/list')
 			}
 		}).then(team => {
 			team.getPlayers().then(players => {
-				res.json(players)
+				if(players.length === 0) {
+					res.json({
+						message: "Nenhum jogador foi adicionado ao time"
+					})
+				}
+				else {
+					res.json(players)
+				}				
 			})
 		})
 	})
