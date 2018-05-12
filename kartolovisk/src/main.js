112c36387d8b94212de0493640f2ebbb2398148c
Rodrigo Miguel <rodrigo.santos@dce.ufpb.br>
2018-05-12 20:22:27 -0300

    correcao das rotas

---
+++ kartolovisk/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import router from './router'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+import 'vue-material/dist/theme/default-dark.css'
+import App from './App'
+
+Vue.use(VueMaterial)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
