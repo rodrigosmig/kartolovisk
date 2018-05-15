54a3d2564e74dc91e8a1d6df553e29cdab7236c4
Cleano <cleano.ferreira@dce.ufpb.br>
2018-05-15 19:45:10 -0300

    listagem por selecao

--- kartolovisk/src/components/Home.vue
+++ kartolovisk/src/components/Home.vue
@@ -57,17 +57,10 @@
             </md-card-content> -->
 
             
-                <div class="md-layout-item md-layout md-gutter">
+              <div class="md-layout-item md-layout md-gutter">
                   <div class="md-layout-item">
-                    <md-card-content>
-                      <md-menu md-size="medium" md-align-trigger>
-                        <md-button md-menu-trigger>Seleção</md-button>
-                        <md-menu-content>
-                          <md-menu-item>My Item 1</md-menu-item>
-                          <md-menu-item>My Item 2</md-menu-item>
-                          <md-menu-item>My Item 3</md-menu-item>
-                        </md-menu-content>
-                      </md-menu>
+                    <md-card-content>                      
+                        <selecao @clickSelecao="searchSelecao"></selecao>
                     </md-card-content>
                   </div>
 
@@ -137,12 +130,14 @@ import axios from 'axios'
 import Posicao from './Posicao'
 import PlayerList from './PlayerList'
 import Formacao from './Formacao'
+import Selecao from './Selecao'
 
 export default {
   components: {
     Posicao,
     PlayerList,
-    Formacao
+    Formacao,
+    Selecao
   },
   name: 'LastRowFixed',
   data(){
@@ -176,6 +171,16 @@ export default {
   methods: {
     search: function(position) {
       const url = "http://localhost:3000/players/position/" + position
+      axios.get(url)
+        .then(response => {
+          this.players = response.data
+        })
+        .catch(error => {
+            console.log("Erro")
+        })
+    },
+    searchSelecao: function(pais){
+      const url = "http://localhost:3000/players/country/" + pais
       axios.get(url)
         .then(response => {
           this.players = response.data
---
+++ kartolovisk/src/components/Selecao.vue
@@ -0,0 +1,72 @@
+<template>
+
+<md-menu md-size="medium" md-align-trigger>
+        <md-button md-menu-trigger>SELEÇÃO</md-button>
+        <md-menu-content>
+            <md-menu-item @click="searchSelecao(index)" 
+            v-for="(p, index) in paises" 
+            :key=index>{{ p }}</md-menu-item>
+        </md-menu-content>
+    </md-menu>
+   
+</template>
+
+<script>
+export default {
+
+     data: () => ({        
+        paises: [
+            'Egito',
+            'Rússia',
+            'Arábia Saudita',
+            'Uruguai',
+            
+            'Irã',
+            'Marrocos',
+            'Portugal',
+            'Espanha',
+
+            'Austrália',
+            'Dinamarca',
+            'França',
+            'Peru',
+
+            'Argentina',
+            'Croácia',
+            'Islândia',
+            'Nigéria',
+
+            'Brazil',
+            'Costa Rica',
+            'Sérvia',
+            'Suíça',
+
+            'Alemanha',
+            'Coreia do Sul',
+            'México',
+            'Suécia',
+
+            'Bélgica',
+            'Inglaterra',
+            'Panamá',
+            'Tunísia',
+
+            'Colômbia',
+            'Japão',
+            'Polônia',
+            'Senegal'
+
+
+            
+        ],        
+    }),
+     methods: {
+        searchSelecao: function(index) {
+            const pais = this.paises[index].toLowerCase()
+            this.$emit("clickSelecao", pais)
+        }
+    }
+    
+  
+}
+</script>
