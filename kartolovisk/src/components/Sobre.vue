c264530d2ba50866e18332f13c644fd9a397ade9
Elenilton <elenilton.ramos@dce.ufpb.br>
2018-05-16 14:52:52 -0300

    sobre feito

--- kartolovisk/src/components/Sobre.vue
+++ kartolovisk/src/components/Sobre.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>TESTA SOBRE</h1>
+  <div>  
+  <h1> SOBRE O KARTOLOVISK</h1>
+<br>
+
+   <h4>O Kartolovisk é um site destinado a apoiar as pessoas que gostam de jogar o Cartola FC promovido pela Globo. Com o intuito de abranger o maior evento do esporte, a copa do mundo. Foi desenvolvido no escopo da disciplina Desenvolvimento de Sistemas Corporativo do sétimo Período do curso de Sistemas de informação.</h4>
+</div>
 </template>
 
 <script>
