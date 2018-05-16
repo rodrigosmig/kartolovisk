df27a2bc14c7c7c2ac701542c86b92265dcefed9
Elenilton <elenilton.ramos@dce.ufpb.br>
2018-05-16 15:28:12 -0300

    retirada das rotas não utilizáveis

--- kartolovisk/src/router/index.js
+++ kartolovisk/src/router/index.js
@@ -1,33 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Home from '@/components/Home'
 import Noticias from '@/components/Noticias'
 import Regras from '@/components/Regras'
 import Sobre from '@/components/Sobre'
 import Login from '@/components/Login'
 import Cadastro from '@/components/Cadastro'
-import Profile from '@/components/Profile'
+
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/hello',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
+
     {
       path: '/',
       name: Login,
       component: Login
-    },
-    {
-      path: '/profile',
-      name: Profile,
-      component: Profile
     },
     {
       path: '/cadastro',
