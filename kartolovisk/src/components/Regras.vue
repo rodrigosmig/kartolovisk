4f1afe254aebe1e5b12ad8512c7d20730a26c93c
WellingtonLemos <wellington.lourenco@dce.ufpb.br>
2018-05-30 12:05:50 -0300

    Criação de todas páginas restantes

--- kartolovisk/src/components/Regras.vue
+++ kartolovisk/src/components/Regras.vue
@@ -1,125 +1,102 @@
 <template>
-<div>      
-<h1>Regras</h1>
-  <h3>
+	<div class="page-container">
+    <md-app md-waterfall md-mode="fixed-last">
+      <md-app-toolbar class="md-large md-dense md-accent">
+        <div class="md-toolbar-row">
 
-O Kartolovisk é um game de futebol que permite que o jogador administre uma seleção próprio. Usando os resultados oficiais de cada partida da copa do mundo, o desempenho dos jogadores nas Seleções reais se transforma em pontos para os mesmos atletas nos times virtuais. Confira na abaixo como funciona este sistema de pontuação.
-</h3>
- <h3>
-Os pontos são recebidos (ou perdidos) de acordo com as atuações dos atletas em campo após cada jogo, através dos itens de scout. A pontuação geral, dada pela média dos pontos dos jogadores de cada time, determina a posição destas equipes no ranking das diferentes ligas nas quais eles estão inseridos, sejam públicas ou privadas.
-</h3>
- <h3>
-No início da Copa do Mundo são levados em consideração qualidades, características e o histórico profissional de cada atleta e técnico. Além disso, a cada rodada as estatísticas de desempenho dos jogadores são consolidadas e transformadas em pontos.
-</h3>
- <h6>
-Confira abaixo a tabela de pontos do Cartola FC:
-</h6>
+					<div class="md-toolbar-section-start">
+            <div class="separator">
+              <md-avatar class="md=large">
+                <img src="../assets/kremlin.png">
+              </md-avatar>
+            </div>
+            <span class="md-title">Kartolovski</span>
+          </div>
 
-<br>
-<br>
-<br>
+          <md-card class="card-menu">
+            <div class="md-toolbar-section-end">
+              <md-button><router-link to='/noticias' id="menu">NOTÍCIAS</router-link></md-button>
+              <md-button><router-link to='/regras' id="menu">REGRAS</router-link></md-button>
+              <md-button><router-link to='/sobre' id="menu">SOBRE</router-link></md-button>
+              <md-button class="md-raised md-accent" ><a id="menu" href="http://localhost:3000/" @click.prevent="logout()">SAIR</a></md-button>
+            </div>
+          </md-card>
+        </div>
+            
+        <div class="md-toolbar-row">
+          <md-tabs class="md-accent" md-sync-route>
+            <md-tab md-label="Meu Time" to="/Home"></md-tab>
+              <md-tab md-label="Liga" to="/Liga"></md-tab>
+              <md-tab md-label="Ranking" to="/Ranking"></md-tab>
+              <md-tab md-label="Configuração" to="/Configuracao"></md-tab>
+          </md-tabs>
+        </div>
+      </md-app-toolbar>
 
-<table border="1">
+      <md-app-content>
+        <div class="md-layout-item md-layout md-gutter">
+          <div class="md-layout-item">
+            <h1>Regras</h1>
+            <p>O Kartolovisk é um game de futebol que permite que o jogador administre uma seleção próprio. Usando os resultados oficiais de cada partida da copa do mundo, o desempenho dos jogadores nas Seleções reais se transforma em pontos para os mesmos atletas nos times virtuais. Confira na abaixo como funciona este sistema de pontuação.</p>
+            <p>Os pontos são recebidos (ou perdidos) de acordo com as atuações dos atletas em campo após cada jogo, através dos itens de scout. A pontuação geral, dada pela média dos pontos dos jogadores de cada time, determina a posição destas equipes no ranking das diferentes ligas nas quais eles estão inseridos, sejam públicas ou privadas.</p>
+            <p>No início da Copa do Mundo são levados em consideração qualidades, características e o histórico profissional de cada atleta e técnico. Além disso, a cada rodada as estatísticas de desempenho dos jogadores são consolidadas e transformadas em pontos.</p>
+          </div>
+          <div class="md-layout-item">
+            <div id="spac">
+              <md-table md-card>
+                <md-table-toolbar>
+                  <h1 class="md-title">Scouts de Ataque</h1>
+                </md-table-toolbar>
+                <md-table-row>
+                  <md-table-head>Scouts</md-table-head>
+                  <md-table-head>Score</md-table-head>
+                </md-table-row>
+                <md-table-row>
+                  <md-table-cell>RB</md-table-cell>
+                  <md-table-cell>89</md-table-cell>
+                </md-table-row>
+              </md-table>
+            </div>
 
-<tr>
-<h3>SCOOUTS DE ATAQUE</h3>
-</tr>
-<tr>
-<th>Scoots</th>
-<th>pontos</th>
-</tr>
-<tr>
-<td>Roubada de bola(RB)</td>
-<td>+1,5</td>
-</tr>
-<tr>
-<td>Falta cometida (FC)</td>
-<td>-0,5</td>
-</tr>
-<tr>
-<td>Gol contra (GC)</td>
-<td>-5,0</td>
-</tr>
-<tr>
-<td>Cartão amarelo (CA)</td>
-<td>-2,0</td>
-</tr>
-<tr>
-<td>Cartão vermelho (CV)</td>
-<td>-5,0</td>
-</tr>
-<tr>
-<td>Jogo sem sofrer gol (SG) - exclusivo para as posições de goleiro, zagueiro e lateral</td>
-<td>+5,0</td>
-</tr>
-<tr>
-<td>Defesa difícil (DD) - exclusivo para a posição de goleiro</td>
-<td>+3,0</td>
-</tr>
-<tr>
-<td>Defesa de pênalti (DP) - exclusivo para a posição de goleiro</td>
-<td>+7,0</td>
-</tr>
-<tr>
-<td>Gol sofrido (GS) - exclusivo para a posição de goleiro</td>
-<td>-2,0</td>
-</tr>
+            <div id="spac">
+              <md-table md-card>
+                <md-table-toolbar>
+                  <h1 class="md-title">Scouts de Defesa</h1>
+                </md-table-toolbar>
+                <md-table-row>
+                  <md-table-head>Scouts</md-table-head>
+                  <md-table-head>Score</md-table-head>
+                </md-table-row>
+                <md-table-row>
+                  <md-table-cell>RB</md-table-cell>
+                  <md-table-cell>89</md-table-cell>
+                </md-table-row>
+              </md-table>
+            </div>
+          </div>
+        </div>
+      </md-app-content>
 
-
-
-<h3>SCOUTS DE ATAQUE</h3>
-<tr>
-<th>Scouts</th>
-<th>pontos</th>
-</tr>
-<tr>
-<td>Falta sofrida (FS)</td>
-<td>+0,5</td>
-</tr>
-<tr>
-<td>Passe errado (PE)</td>
-<td>-0,3</td>
-</tr>
-<tr>
-<td>Assistência (A)</td>
-<td>+5,0</td>
-</tr>
-<tr>
-<td>Finalização na trave (FT)</td>
-<td>+3,0</td>
-</tr>
-<tr>
-<td>Finalização defendida (FD)</td>
-<td>+1,2</td>
-</tr>
-<tr>
-<td>Finalização para fora (FF)</td>
-<td>+0,8</td>
-</tr>
-<tr>
-<td>Gol (G)</td>
-<td>+8,0</td>
-</tr>
-<tr>
-<td>Impedimento (I)</td>
-<td>-0,5</td>
-</tr>
-<tr>
-<td>Pênalti perdido (PP)</td>
-<td>-4,0</td>
-</tr>
-</table>
-
-
-</div>
-
-
-	
+    </md-app>
+  </div>
 </template>
 
-<script>
-export default {
-  
-}
-</script>
+<style>
+	.md-app {
+    max-height: 700px;
+    border: 1px solid rgba(#000,.12);
+  }
+	.card-menu{
+    width: 70%;
+  }
+	#menu{
+    color:white;
+  }
+  #spac{
+    padding-top: 30px
+  }
+</style>
 
+<script>
+	
+</script>
--- kartolovisk/src/components/Sobre.vue
+++ kartolovisk/src/components/Sobre.vue
@@ -1,18 +1,62 @@
 <template>
-  <div>  
-  <h1> SOBRE O KARTOLOVISK</h1>
-  
-<br>
+	<div class="page-container">
+    <md-app md-waterfall md-mode="fixed-last">
+      <md-app-toolbar class="md-large md-dense md-accent">
+        <div class="md-toolbar-row">
 
-    
+					<div class="md-toolbar-section-start">
+            <div class="separator">
+              <md-avatar class="md=large">
+                <img src="../assets/kremlin.png">
+              </md-avatar>
+            </div>
+            <span class="md-title">Kartolovski</span>
+          </div>
 
-   <h4>O Kartolovisk é um site destinado a apoiar as pessoas que gostam de jogar o Cartola FC promovido pela Globo. Com o intuito de abranger o maior evento do esporte, a copa do mundo. Foi desenvolvido no escopo da disciplina Desenvolvimento de Sistemas Corporativo do sétimo Período do curso de Sistemas de informação.</h4>
-</div>
+          <md-card class="card-menu">
+            <div class="md-toolbar-section-end">
+              <md-button><router-link to='/noticias' id="menu">NOTÍCIAS</router-link></md-button>
+              <md-button><router-link to='/regras' id="menu">REGRAS</router-link></md-button>
+              <md-button><router-link to='/sobre' id="menu">SOBRE</router-link></md-button>
+              <md-button class="md-raised md-accent" ><a id="menu" href="http://localhost:3000/" @click.prevent="logout()">SAIR</a></md-button>
+            </div>
+          </md-card>
+        </div>
+            
+        <div class="md-toolbar-row">
+          <md-tabs class="md-accent" md-sync-route>
+            <md-tab md-label="Meu Time" to="/Home"></md-tab>
+              <md-tab md-label="Liga" to="/Liga"></md-tab>
+              <md-tab md-label="Ranking" to="/Ranking"></md-tab>
+              <md-tab md-label="Configuração" to="/Configuracao"></md-tab>
+          </md-tabs>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-content>
+      	<h1>Sobre</h1>
+      	<p>Em virtude deste ano de 2018, ocorrer a copa do mundo, que é o maior evento de futebol do planeta, pensamos em criar um jogo no estilo do Cartola FC, mas que seja voltado para a copa do mundo de futebol.</p>
+      	<p>Desenvolver um jogo intuitivo no estilo de um fantasy game, em que o usuário possa escalar sua própria seleção da copa do mundo, baseado nos jogadores das seleções que estão convocadas para a copa do mundo de 2018</p>
+      </md-app-content>
+
+    </md-app>
+  </div>
 </template>
 
+<style>
+	.md-app {
+    max-height: 700px;
+    border: 1px solid rgba(#000,.12);
+  }
+	.card-menu{
+    width: 70%;
+  }
+	#menu{
+    color:white;
+  }
+</style>
+
 <script>
-export default {
-  
-}
+	
 </script>
 
